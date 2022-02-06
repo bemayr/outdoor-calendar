5a1e96dbe91c633eae27af790816ae2b4bbbfe0b
bemayr <bernhard@mayr.io>
2022-02-06 12:24:52 +0100

    Implemented PoC

---
+++ EventlistToICal/Event.cs
@@ -0,0 +1,33 @@
+﻿using Npoi.Mapper.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventlistToICal
+{
+    internal class Event
+    {
+        [Column(0)]
+        public string Title { get; set; }
+
+        [Column(1)]
+        public string Link { get; set; }
+
+        [Column(2)]
+        public string Kind { get; set; }
+
+        [Column(3)]
+        public string Type { get; set; }
+
+        [Column(4)]
+        public string Country { get; set; }
+
+        [Column(5)]
+        public DateTime From { get; set; }
+
+        [Column(6)]
+        public DateTime To { get; set; }
+    }
+}
---
+++ EventlistToICal/Program.cs
@@ -0,0 +1,51 @@
+﻿using EventlistToICal;
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+using Npoi.Mapper;
+
+var mapper = new Mapper("events.xlsx");
+
+var result = mapper.Take<Event>("2022")
+    .Select(row => row.Value)
+    .Where(e => e.From != new DateTime())
+    .Select(e => {
+        e.Kind = e.Kind switch
+        {
+            "O" => "Orienteering",
+            "AR" => "Adventure Race",
+            "RG" => "Rogaining",
+            "MM" => "Mountain Marathon",
+            "S" => "Survival",
+            "TR" => "Trail Run",
+            _ => e.Kind
+        };
+        e.Type = e.Type switch
+        {
+            "S" => "Single",
+            "T" => "Team",
+            _ => e.Type
+        };
+
+        return e;
+    })
+    .Select(e => new CalendarEvent
+    {
+        Summary = e.Title,
+        Description = $"{e.Kind} ({e.Type})\r\n{e.Link}",
+        Location = e.Country,
+        Start = new CalDateTime(e.From),
+        End = new CalDateTime(e.To),
+        IsAllDay = true,
+    });
+
+var calendar = new Calendar();
+calendar.AddProperty("X-WR-CALNAME", "Outdoor Sport Events");
+calendar.Events.AddRange(result);
+
+var icsString = new CalendarSerializer().SerializeToString(calendar);
+
+await File.WriteAllTextAsync("events.ics", icsString);
+
+Console.WriteLine("Finished... 🏁");
